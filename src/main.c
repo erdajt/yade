a44c9cee9ab2369be96733d347e4f893950e71ba
erdajt <erdajtsopjani.tech@gmail.com>
2025-12-09 01:42:03 +0100

    feat: add inline code lexing in main
    - handle `-i`/`--inline` argument for inline code input
    - initialize lexer and iterate tokens until `TOKEN_EOF`

--- src/main.c
+++ src/main.c
@@ -1,14 +1,50 @@
-#include "yade/print.h"
+#include <stdio.h>
 #include <string.h>
 
+#include "yade/lexer.h"
+#include "yade/print.h"
+
 int main(int argc, char *argv[]) {
-  if (argc < 2 || strcmp(argv[1], "-h")) {
+  const char *arg1 = argv[1];
+  if (argc < 2 || (strcmp(arg1, "-h") == 0)) {
     print_help();
     return 0;
   }
 
   print_banner();
 
-  print_error("Invalid Arguments");
+  char *code;
+
+  if (strcmp(arg1, "-i") == 0 || strcmp(arg1, "--inline") == 0) {
+    if (argc < 3) {
+      print_error("yade: missing code after %s\n", arg1);
+      return 1;
+    }
+    code = argv[2];
+  }
+
+  yade_lexer lexer;
+  lexer_init(&lexer, code); // NOLINT
+
+  yade_token token;
+  for (;;) {
+    token = lexer_get_next_token(&lexer);
+
+    if (token.type == TOKEN_INVALID) {
+      print_error("Invalid token: %c at position: %zu\n", lexer.current_char,
+                  lexer.position);
+      return 1;
+    }
+
+    printf("token name: %s, token type: %d\n",
+           lexer_token_type_name(token.type), token.type);
+
+    if (token.type == TOKEN_EOF) {
+      break;
+    }
+
+    // printf(*expr);
+  }
+
   return 0;
 }
